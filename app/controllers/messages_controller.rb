8b810c5aa1b12685b02e03690d7410d3719464f7
future21star <future21star@yahoo.com>
2017-12-08 15:19:15 -0500

    add use method for messages for actioncable

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -25,6 +25,16 @@ class MessagesController < ApplicationController
   def delte
   end
 
+   def use
+    message = Message.new(message_params)
+    message.user = current_user
+    if message.save
+      ActionCable.server.broadcast 'messages',
+        message: message.content,
+        user: message.user.name
+      head :ok
+    end     
+   end
 private
   def message_params
     params.require(:message).permit(:content, :chatroom_id)
